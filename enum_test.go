4825214882875c586d24c43fe40d7add4fe1ba6b
kkkunny <1401289859@qq.com>
2025-04-05 09:46:41 +0800

    add unit test

---
+++ enum_test.go
@@ -0,0 +1,46 @@
+package enum
+
+import "testing"
+
+func TestNewString(t *testing.T) {
+	v := New[struct {
+		A string
+		B string
+	}]()
+	if v.A != "A" {
+		t.FailNow()
+	}
+	if v.B != "B" {
+		t.FailNow()
+	}
+}
+
+func TestNewInteger(t *testing.T) {
+	v := New[struct {
+		Zero int64
+		One  int64
+	}]()
+	if v.Zero != 0 {
+		t.FailNow()
+	}
+	if v.One != 1 {
+		t.FailNow()
+	}
+}
+
+func TestNewInteger2(t *testing.T) {
+	v := New[struct {
+		Zero int64
+		one  int64
+		Two  int64
+	}]()
+	if v.Zero != 0 {
+		t.FailNow()
+	}
+	if v.one != 0 {
+		t.FailNow()
+	}
+	if v.Two != 2 {
+		t.FailNow()
+	}
+}
